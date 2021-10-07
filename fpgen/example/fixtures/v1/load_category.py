4f6cc96d9cfe9029bd96c07c893395879751b647
filipp.zotov <filipp.zotov@intexsoft.by>
2021-10-07 12:19:15 +0300

    FPGEN-1: Added singleton declaration for database class. Added autoload for json type catalog and base type attributes models.

--- fpgen/example/fixtures/v1/load_category.py
+++ fpgen/example/fixtures/v1/load_category.py
@@ -1,19 +1,9 @@
 from fpgen.abstract_fixture_loader import AbstractFixtureLoader
-from fpgen.example.database import db
 from fpgen.example.models.goods import Category
 
 
 class LoadCategory(AbstractFixtureLoader):
-    def load(self) -> None:
-        objects = [
-            Category(
-                name=self.fake.sentence(nb_words=4, variable_nb_words=True),
-                description=self.fake.sentence(nb_words=4, variable_nb_words=True)
-            )
-            for _ in range(self.quantity)
-        ]
-        with db.session_scope() as session:
-            session.bulk_save_objects(objects)
+    __model__ = Category
 
     @staticmethod
     def env_group() -> list:
--- fpgen/example/fixtures/v1/load_unit.py
+++ fpgen/example/fixtures/v1/load_unit.py
@@ -1,18 +1,9 @@
-import json
-
 from abstract_fixture_loader import AbstractFixtureLoader
-from fpgen.example.database import db
 from fpgen.example.models.goods import Unit
 
 
 class LoadUnit(AbstractFixtureLoader):
-    def load(self) -> None:
-        with open(self.catalog, "r") as response:
-            units = json.loads(response.read())
-            units = units[:self.quantity] if self.quantity else units
-            objects = [Unit(**unit) for unit in units]
-            with db.session_scope() as session:
-                session.bulk_save_objects(objects)
+    __model__ = Unit
 
     @staticmethod
     def env_group() -> list:
